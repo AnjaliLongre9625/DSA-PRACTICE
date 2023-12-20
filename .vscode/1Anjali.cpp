83ed094669a1d9b24f110d4d4fa9ee9e4c9876d5
AnjaliLongre9625 <132768572+AnjaliLongre9625@users.noreply.github.com>
2023-12-20 13:04:58 +0530

    first commit

---
+++ .vscode/1Anjali.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"WISHE ME LUCK"<<"=:"<<"ALL THE BEST ANJALI FOR YOUR END SEM ";
+}
---
+++ .vscode/New folder/41length2d.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int length(char input[]){
+int count=0;
+for(int i=0;input[i]!='\0';i++){
+    
+    count++;
+}
+return count;
+}
+
+int main ()
+{   char input[100];
+    
+    cin.getline(input,7,'\o');
+    cout<<input;
+  
+}
